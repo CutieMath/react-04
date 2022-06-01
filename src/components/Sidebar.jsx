6b9612147d2275b8e3d4d8a757d986a7131ed116
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-01 20:50:13 +1000

    add sub menu to the dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,11 @@ import { links } from "../data/dummy";
 
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = `flex items-center gap-5 
+  pl-4 pt-3 pb-2.5 rounded-lg text-while text-md m-2`;
+  const normalLink = `flex items-center gap-5 
+  pl-4 pt-3 pb-2.5 rounded-lg text-while text-md 
+  text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2`;
   return (
     <div
       className="ml-3 h-screen 
@@ -42,6 +47,19 @@ const Sidebar = () => {
             {links.map((item) => (
               <div key={item.title}>
                 <p className="text-gray-400 m-3 m-4 uppercase">{item.title}</p>
+                {item.links.map((link) => (
+                  <NavLink
+                    key={link.name}
+                    to={`/${link.name}`}
+                    onClick={() => {}}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
               </div>
             ))}
           </div>
