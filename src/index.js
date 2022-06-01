b6ccb3b75584da2805b63a0cfbd022ae72791813
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-01 21:01:23 +1000

    use a context provider to set global states

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
+// the context
+import { ContextProvider } from "./contexts/ContextProvider";
 
 const MOUNT = document.getElementById("root");
 
-ReactDOM.render(<App />, MOUNT);
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  MOUNT
+);
