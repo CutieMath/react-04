e7cb0ccec4825801c61095bd7e4e435e30946453
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-02 16:33:03 +1000

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import avatar from "../data/avatar.jpg";
-import { Cart, Chart, Notification, UserProfile } from ".";
+import { Cart, Chat, Notification, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
@@ -76,7 +76,7 @@ const Navbar = () => {
           </div>
         </TooltipComponent>
         {isClicked.cart && <Cart />}
-        {isClicked.chart && <Chart />}
+        {isClicked.chat && <Chat />}
         {isClicked.userProfile && <UserProfile />}
         {isClicked.notification && <Notification />}
       </div>
