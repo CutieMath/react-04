a7e288f69cf45f0bdf42ba747ce983f1a2273401
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-03 12:10:07 +1000

    add sparkline

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -95,6 +95,17 @@ const Home = () => {
                 </p>
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="#957DAD"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="#957DAD"
+                />
+              </div>
             </div>
           </div>
         </div>
