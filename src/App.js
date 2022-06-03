d18def3eb1e8e02ee9cb463b3e11d3d3c8f7d0bc
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-03 16:07:22 +1000

    finish kanban board

--- src/App.js
+++ src/App.js
@@ -73,9 +73,9 @@ const App = () => {
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/customers" element={<Customers />} />
                 {/* Other Apps */}
+                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
                 {/* Charts */}
                 <Route path="/line" element={<Line />} />
