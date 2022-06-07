e1932452084e0cfbb7f7c9f93be804ff3529493e
Yuxin Ye <yuxin_ye@protonmail.com>
2022-06-07 10:54:18 +1000

    add barchart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -14,9 +14,6 @@ import {
   areaPrimaryYAxis,
   areaPrimaryXAxis,
 } from "../../data/dummy";
-
-import { useStateContext } from "../../contexts/ContextProvider";
-
 function Area() {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
